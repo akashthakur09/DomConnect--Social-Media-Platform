963320d986643f1697c383194fc2752d1767d0bb
Akash Thakur <akash0218.nitt@gmail.com>
2024-05-01 16:25:15 +0530

    edit frontend

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -2,7 +2,7 @@ import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
-
+import { Link } from 'react-router-dom';
 const Home = ({history}) => {
 
 const nav = useNavigate();
@@ -18,8 +18,10 @@ const nav = useNavigate();
     <div className="jumbotron centered align-items-center">
     <div className="container  ">
     <h1 className="display-3 mt-3 mb-3">DomConnect</h1>
-    <a className="btn btn-light btn-lg mr-2" href="/register" role="button">Register</a>
-    <a className="btn btn-dark btn-lg" href="/login" role="button">Login</a>
+    {/* <a className="btn btn-light btn-lg mr-2" href="/register" role="button">Register</a>
+    <a className="btn btn-dark btn-lg" href="/login" role="button">Login</a> */}
+    <Link to="/register" className="btn btn-light btn-lg mr-2" role="button">Register</Link>
+    <Link to="/login" className="btn btn-dark btn-lg" role="button">Login</Link>
   </div>
   </div>
 </div>
