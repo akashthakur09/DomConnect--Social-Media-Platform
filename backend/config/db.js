7fc4812f952361b7c26f0426768db33f9f54718f
Akash Thakur <akash0218.nitt@gmail.com>
2024-05-01 13:33:09 +0530

    Second commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,10 @@
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 
+dotenv.config();
 
-url="mongodb://localhost:27017/DomConnect"
-;
+const url = process.env.MONGO_URL;
 
-
-mongoose.connect(url , (err)=>{
-if (err)
-{
-    console.log(err)
-}
-else
-{
-    console.log("connected Success")
-}
-
-} )
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to the database:", err));
