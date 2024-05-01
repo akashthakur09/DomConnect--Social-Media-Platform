ec3fa07a8d35ac98edfedd4150da14686f46173b
Akash Thakur <akash0218.nitt@gmail.com>
2024-05-01 22:24:55 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,13 @@ app.use(
 app.use(bodyParser.json());
 
 app.use(cors({ origin: true }));
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 // app.use(cors());
 // Passport middleware
 app.use(passport.initialize());
