4293aa306cd6bf1b5429321cdbb6c6b4241886ce
Akash Thakur <akash0218.nitt@gmail.com>
2024-05-01 15:00:59 +0530

    deploying the website on render

--- backend/server.js
+++ backend/server.js
@@ -82,17 +82,21 @@ io.on("connection", (socket) => {
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/FrontEnd/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/FrontEnd/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "FrontEnd", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "FrontEnd", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
+
+
+
+app.use(express.static(path.join(__dirname, "build")));
 
 // --------------------------deployment------------------------------
 
